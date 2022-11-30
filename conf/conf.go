d8061c966399a1b245ab14fd62b00c31baf78095
oleh.levytskyi <Alik346alik>
2022-11-30 15:03:47 +0100

    feature/001-bitcoin-rate-app: init commit with Bitcoin rate application

--- conf/conf.go
+++ conf/conf.go
@@ -15,6 +15,54 @@
 
 package conf
 
-//
-// Todo: Define anything for configuration like structures and methods to read and process configuration
-//
+import (
+	"github.com/eliona-smart-building-assistant/go-utils/db"
+	"strconv"
+	"time"
+)
+
+type Currency struct {
+	Code        string
+	Description string
+	ProjectId   string
+}
+
+// ReadCurrencies reads all configured currencies and send each currency to the given channel
+func ReadCurrencies(currencies chan Currency) error {
+	err := db.Query(db.Pool(), "select code, description, coalesce(proj_id,'') from bitcoin.currencies", currencies)
+	return err
+}
+
+// PollingInterval returns the interval polling the bitcoin rate api
+func PollingInterval() time.Duration {
+	interval, _ := strconv.Atoi(get("polling_interval", "10"))
+	return time.Duration(interval) * time.Second
+}
+
+// Endpoint returns the configured API endpoint to get bitcoin rates.
+func Endpoint() string {
+	return get("endpoint", "https://api.coindesk.com/v1/bpi/currentprice.json")
+}
+
+// Value returns the configuration string referenced by key. The configuration is stored in the init
+// table bitcoin.configuration. This table should be configurable via the eliona frontend.
+func get(name string, fallback string) string {
+	valueChan := make(chan string)
+	go func() {
+		_ = db.Query(db.Pool(), "select value from bitcoin.configuration where name = $1", valueChan, name)
+	}()
+	value := <-valueChan
+	if len(value) == 0 {
+		return fallback
+	}
+	return value
+}
+
+// Set sets the value of configuration
+func Set(connection db.Connection, name string, value string) error {
+	return db.Exec(connection, "insert into bitcoin.configuration (name, value) values ($1, $2) on conflict (name) do update set value = excluded.value", name, value)
+}
+
+func InsertCurrency(connection db.Connection, currency Currency) error {
+	return db.Exec(connection, "insert into bitcoin.currencies (code, description, proj_id) values ($1, $2, $3)", currency.Code, currency.Description, currency.ProjectId)
+}
